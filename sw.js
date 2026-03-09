0cbfac186c742fbf251f36ad3619523c1cde4dc0
lamviec246357-glitch <lamviec246357@gmail.com>
2026-03-09 12:36:36 +0700

    Add service worker for caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "giaoviec-cache-v1";
+
+const urlsToCache = [
+"./",
+"./index.html",
+"./manifest.json",
+"./icon.svg"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
